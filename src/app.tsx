4ec121ebc90f93c9dbbc0261622b4cdec8bdfe42
Yinka Adedire <yinkakun@gmail.com>
2021-08-15 18:42:04 +0100

    implement routes and app base styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './app.css';
+import Sidebar from './components/sidebar';
+import ComingSoon from './components/coming-soon';
+import Expenses from './pages/expenses';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="app-container">
+      <Sidebar />
+      <Switch>
+        <Route path="/expenses" component={Expenses} />
+        <Route path="*">
+          <ComingSoon />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
